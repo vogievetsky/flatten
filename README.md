f804d2364fd2e883791caa3b0507111f4ae089da
Vadim Ogievetsky <vadimon@gmail.com>
2013-03-31 19:15:27 -0700

    wrapped up

--- README.md
+++
@@ -1,4 +0,0 @@
-troll2013
-=========
-
-April Fools Day 2013
--- local.js
+++ local.js
@@ -1,19 +1,23 @@
 (function() {
-  function isLolTime() {
-    var now = new Date(); // Make sure it is April 1st between 9 and 5
-    return (now.getMonth() === 3 && now.getDate() === 1 && 9 <= now.getHours() && now.getHours() < 5)
-  }
-  if (!isLolTime()) return;
-  var t = document.createElement('script');
-  t.type = 'text/javascript';
-  t.async = true;
-  t.src = 'https://raw.github.com/wesbos/aprilFools.css/master/aprilFools.css';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-  window.mxUserStyle = function(user, style) {
+  try {
+    function isLolTime() {
+      var now = new Date(); // Make sure it is April 1st between 9 and 5
+      return (now.getMonth() === 3 && now.getDate() === 1 && 9 <= now.getHours() && now.getHours() < 5)
+    }
     if (!isLolTime()) return;
-    user = user + '@meta';
-    if (mmx.gv.userInfo().email.indexOf(user) !== 0) return;
-    d3.select('head').append('style').attr('type', 'text/css').text(style);
-  };
+    var t = document.createElement('script');
+    t.type = 'text/javascript';
+    t.async = true;
+    t.src = 'https://raw.github.com/wesbos/aprilFools.css/master/aprilFools.css';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+    window.mxUserStyle = function(user, style) {
+      try {
+        if (!isLolTime()) return;
+        user = user + '@meta';
+        if (mmx.gv.userInfo().email.indexOf(user) !== 0) return;
+        d3.select('head').append('style').attr('type', 'text/css').text(style);
+      } catch(e) { }
+    };
+  } catch(e) { }
 })();
