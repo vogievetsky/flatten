e00aa19055c9250d9a6bf1d80e55af2e15172e6d
Vadim Ogievetsky <vadimon@gmail.com>
2013-03-31 19:45:06 -0700

    final changed and tweaks

--- local.js
+++ local.js
@@ -1,23 +1,30 @@
 (function() {
   try {
-    function isLolTime() {
+    function isGoodTime() {
       var now = new Date(); // Make sure it is April 1st between 9 and 5
-      return (now.getMonth() === 3 && now.getDate() === 1 && 9 <= now.getHours() && now.getHours() < 5)
+      //return true; // For testing
+      return (now.getMonth() === 3 && now.getDate() === 1 && 9 <= now.getHours() && now.getHours() < 17)
     }
-    if (!isLolTime()) return;
-    var t = document.createElement('script');
-    t.type = 'text/javascript';
-    t.async = true;
-    t.src = 'https://raw.github.com/vogievetsky/flatten/master/remote.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+    if (!isGoodTime()) return;
+    if (mmx.gv.userInfo().email.indexOf('@meta') === -1) return;
     window.mxUserStyle = function(user, style) {
       try {
-        if (!isLolTime()) return;
+        if (!isGoodTime()) return;
         user = user + '@meta';
         if (mmx.gv.userInfo().email.indexOf(user) !== 0) return;
-        d3.select('head').append('style').attr('type', 'text/css').text(style);
+
+        var ns = document.createElement('style');
+        ns.type = 'text/css';
+        ns.innerText = style;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ns, s);
       } catch(e) { }
     };
+    var ns = document.createElement('script');
+    ns.type = 'text/javascript';
+    ns.async = true;
+    ns.src = 'https://raw.github.com/vogievetsky/flatten/master/remote.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ns, s);
   } catch(e) { }
 })();
